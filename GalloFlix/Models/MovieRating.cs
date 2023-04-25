0a3ba77fad073f7790d882c121987b46b289c5b5
Mariah <liviamariahzaratini@gmail.com>
2023-04-25 12:29:11 -0300

    Criada a pasta Data e a classe de Contexto

--- GalloFlix/Models/MovieRating.cs
+++ GalloFlix/Models/MovieRating.cs
@@ -1,12 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GalloFlix.Models
-{
+
+namespace GalloFlix.Models;
+
+[Table("MovieRating")]
     public class MovieRating
-    {
-        
-    }
+{
+    [Key, Column(Order = 1)]
+    public int MovieId {get;set;} 
+    [ForeignKey("MovieId")]
+    public Movie Movie {get;set;}
+
+
+    [Key, Column(Order = 2)]
+    public string UserId {get;set;}
+    [ForeignKey("UserId")]     
+    public AppUser User {get;set;}
+
+    [Required]
+    public byte RatingValue {get;set;}
+
+    [Required]
+    public DateTime RatingDate {get;set;}  
 }
